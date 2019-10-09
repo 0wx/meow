bbde3c0885480e62acb3ab9b149a54b6596f7363
nyaaa <callmeoniichandesu@gmail.com>
2019-10-09 06:26:47 +0000

    Create main.py
    Main Py

---
+++ main.py
@@ -0,0 +1,23 @@
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+
+def get_url():
+    contents = requests.get('http://aws.random.cat/meow').json()    
+    url = contents['file']
+    return url
+
+def meow(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('636331007:AAHmOAXcdhEmZ0kiTX6LvbFI3153WH8Drro')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('meow',meow))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
